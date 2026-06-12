2502adb4c5e494cf4b0f80a436b8959d4ef6fc69
Ansh Kumar <ansh.kumar2025@glbajajgroup.org>
2026-06-12 12:00:00 +0530

    Add Day 4

---
+++ DAY 4/Q16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int start, end;
+
+    printf("Enter starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter ending number: ");
+    scanf("%d", &end);
+
+    printf("Armstrong numbers between %d and %d are:\n", start, end);
+
+    for (int i = start; i <= end; i++) {
+        int num = i;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        if (sum == i)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
