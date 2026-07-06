e65fad1aa6d4fc6994f4a97ff5277d8e47b51baa
Ansh Kumar <ansh.kumar2025@glbajajgroup.org>
2026-07-06 12:00:00 +0530

    Add Day 28

---
+++ DAY 28/Q110.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, key, low, high, mid, found = 0;
+
+    printf("Enter size of sorted array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d sorted elements:\n", n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+        {
+            found = 1;
+            break;
+        }
+        else if(arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    if(found)
+        printf("Element found at position %d", mid + 1);
+    else
+        printf("Element not found.");
+
+    return 0;
+}
